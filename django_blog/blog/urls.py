1ac2aa3be82cd2ade7bf7f4378a60991bc61f1c4
vlajksanyee <vlajksanyi@gmail.com>
2025-03-09 06:31:27 +0100

    feat(about): add about path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
