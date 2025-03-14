473bf57ee459c58623f694a424d4b631d7d9f0e6
vlajksanyee <vlajksanyi@gmail.com>
2025-03-14 06:37:17 +0100

    test(blog): display test posts on home page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'AlexB',
+        'title': 'Blog Post #1',
+        'content': 'This is my first post!',
+        'date_posted': 'March 14, 2025'
+    },
+    {
+        'author': 'DianaB',
+        'title': 'Blog Post #2',
+        'content': 'This is Diana\'s first post!',
+        'date_posted': 'March 14, 2025'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
