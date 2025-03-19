296458e2a4b5281485b17ab59b07fa7bdbf9e21c
vlajksanyee <vlajksanyi@gmail.com>
2025-03-19 06:28:47 +0100

    feat(blog): display posts from database

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,23 +1,10 @@
+from .models import Post
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'AlexB',
-        'title': 'Blog Post #1',
-        'content': 'This is my first post!',
-        'date_posted': 'March 14, 2025'
-    },
-    {
-        'author': 'DianaB',
-        'title': 'Blog Post #2',
-        'content': 'This is Diana\'s first post!',
-        'date_posted': 'March 14, 2025'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
