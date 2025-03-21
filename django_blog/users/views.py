c2489ff22a45855df47ece4baea00d4d68aa0ee9
vlajksanyee <vlajksanyi@gmail.com>
2025-03-21 06:33:38 +0100

    feat(users): create custom registration form with email field

--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,14 +1,15 @@
+from .forms import UserRegisterForm
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save() # Saves the new user to the database
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
