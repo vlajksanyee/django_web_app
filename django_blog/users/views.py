da2e0a448b4060c163a9f1c80e5271cf9d30f59b
vlajksanyee <vlajksanyi@gmail.com>
2025-03-28 06:09:01 +0100

    feat(users): implement profile info update

--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,10 +1,11 @@
 from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 from django.shortcuts import redirect, render
 
 
-def register(request):
+def register(request: HttpRequest):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -18,9 +19,18 @@ def register(request):
 
 
 @login_required
-def profile(request):
-    user_form = UserUpdateForm()
-    profile_form = ProfileUpdateForm()
+def profile(request: HttpRequest):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Account info updated!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'user_form': user_form,
