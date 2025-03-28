930210d4e9ca0df58df16def51a376234736dfb6
vlajksanyee <vlajksanyi@gmail.com>
2025-03-28 05:56:07 +0100

    feat(users): add forms for updating profile

--- django_blog/users/forms.py
+++ django_blog/users/forms.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -9,3 +10,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,4 +1,4 @@
-from .forms import UserRegisterForm
+from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
@@ -19,4 +19,12 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    user_form = UserUpdateForm()
+    profile_form = ProfileUpdateForm()
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'users/profile.html', context)
